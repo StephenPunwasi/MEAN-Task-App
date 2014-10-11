4bcdaa12e6f11ad8fae7ff059aa6eafadf4c5c94
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-11 09:15:29 -0400

    Express Routes Added

--- server.js
+++ server.js
@@ -19,6 +19,59 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type:'application/vnd.api+json'})); //parse application/json
 app.use(methodOverride());
 
+// define model =====================================
+var Todo = mongoose.model('Todo', {
+    text: String
+});
+
+// Express routes ===================================
+  // api ============================================
+  app.get('/api/todos', function(req, res){
+      //use mongoose to get all todos in the database
+      Todo.find(function(err, todos){
+          if (err)
+            res.send(err) //error catch
+          res.json(todos); //return all todos in JSON
+      });
+  });
+
+  //CREATE todo and send back all todos after
+  app.post('/api/todos', function(req, res){
+
+      //create a todo, information comes from AJAX request
+      Todo.create({
+          text: req.body.text,
+          done: false
+        }, function(err, todo){
+          if (err)
+            res.send(err);
+
+          //get and return all the todos after you create another
+          Todo.find(function(err, todos){
+              if(err)
+                res.send(err);
+              res.json(todos);
+          });
+      });
+  });
+
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function(req, res){
+      Todo.remove({
+          _id: req.params.todo_id
+        }, function(err, todo) {
+          if (err)
+            res.send(err);
+
+          // get and return all the todos after you create another one
+          Todo.find(function(err, todos){
+              if (err)
+                res.send(err);
+              res.json(todos);
+          });
+      });
+  });
+
 // listen (start app with node server.js) ===========
 
 app.listen(3000);
